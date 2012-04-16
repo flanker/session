ee09eee05fc66f339875d7080780c211703bfe0e
FENG Zhichao <flankerfc@gmail.com>
2012-04-16 22:23:55 +1000

    add validation for post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,1 +1,14 @@
+require 'spec_helper'
 
+describe Post do
+
+  describe 'validation' do
+
+    it 'should not be valid when title is empty' do
+      @post = Post.new
+      @post.should_not be_valid
+    end
+
+  end
+
+end
